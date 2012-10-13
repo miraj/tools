8ad37d441e440992242f8fb498ac24f90c2e3e7f
m <miraj@LT010432.cts.com>
2012-10-13 23:10:24 +0530

    Shell script basic template done

--- templates/shellscript.sh
+++ templates/shellscript.sh
@@ -1,14 +1,42 @@
 #!/bin/bash
 
-for arg in $*
-do
-	case $arg in
-		-f)
-			file=
-		-t)
-			force="yes"
-			;;
-	esace
+usage ()
+{
+	echo
+	echo "Usage:"
+    echo "   $0  <options>"
+	echo
 
+}
+
+file=''
+verbose=no
+debug=no
+
+while [[ $# -ne 0 ]] 
+do
+
+	case $1 in
+		-f)
+			file=$2
+			shift
+			;;
+		-v)
+			verbose=yes
+			;;
+		-d)
+			debug=yes
+			;;
+		 *)
+            echo Unknown option:  $arg
+			;;
+	esac
+	shift
 done
+
+
+[[ $file == '' ]] && { echo File Must be specified; usage; exit 2; }
+
+## More validations to be done here 
+
 
